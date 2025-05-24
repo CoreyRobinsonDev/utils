36543b6b4aee0182438243be11b470dd19cf2f47
Corey Robinson <coreyrob52@gmail.com>
2025-05-24 10:14:44 -0500

    feat: create error handling functions

--- go/main.go
+++ go/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
-	println("Hello, World!")
+
+
+var e = new(ErrorHandler)
+
+func SetErrorHandler(fn func (error)) {
+	e.HandleError = fn
 }
+
+type ErrorHandler struct {
+	HandleError func (error)
+}
+
+
+func Unwrap[T any](val T, err error) T {
+	if err != nil {
+		e.HandleError(err)
+	}
+
+	return val
+}
+
+func UnwrapOr[T any](val T, err error) func(T) T {
+	if err != nil {
+		return func(d T) T {
+			return d
+		}
+	} else {
+		return func(_ T) T {
+			return val
+		}
+	}
+}
+
+func UnwrapOrElse[T any](val T, err error) func(func() T) T {
+	if err != nil {
+		return func(fn func() T) T {
+			return fn()
+		}
+	} else {
+		return func(_ func() T) T {
+			return val
+		}
+	}
+
+}
+
+func Expect(err error) {
+	if err != nil {
+		e.HandleError(err)
+	}
+}
+
